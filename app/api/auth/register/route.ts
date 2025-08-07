1aff6bc561d6a587c24fe2c90ac27d7a5bccba25
cc <spuncheshirecat@gmail.com>
2025-08-07 11:00:22 +0700

    .

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -43,7 +43,7 @@ export async function POST(req: NextRequest) {
 				email,
 				username,
 				displayName: displayName || username,
-				password: hashedPassword,
+				hashedPassword: hashedPassword,
 			},
 			select: {
 				id: true,
